ea4c3dec862e7f4009df001cbe29114feb97d506
irchuri <d4riakert@ya.ru>
2023-10-11 10:34:54 +0200

    потраченного времени жаль

---
+++ алгоритмы и структуры данных/Сортировка пузырьком.py
@@ -0,0 +1,9 @@
+spisok = list(map(int, input().split()))
+flag = True
+while flag:
+    flag = False
+    for i in range(0, len(spisok) - 1):
+        if spisok[i] > spisok[i + 1]:
+            spisok[i], spisok[i + 1] = spisok[i + 1], spisok[i]
+            flag = True
+print(*spisok, sep=' ')
