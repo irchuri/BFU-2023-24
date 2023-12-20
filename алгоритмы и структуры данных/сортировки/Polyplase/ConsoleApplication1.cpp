f2bcb6a2dda0eceebefd5f7f38e25afcb1da137e
irchuri <d4riakert@ya.ru>
2023-12-20 04:01:42 +0200

    потраченного времени жаль

---
+++ алгоритмы и структуры данных/сортировки/Polyplase/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    for (int i = 1000; i>=0; i = i -7)
+    std::cout << i << std::endl;
+    
+}
