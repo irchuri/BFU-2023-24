13f35b31cbe151eae04c2c42355887acea0120f9
irchuri <d4riakert@ya.ru>
2024-02-21 15:52:25 +0200

    потраченного времени жаль

--- алгоритмы и структуры данных/graphs/DFS/DFS4.py
+++ алгоритмы и структуры данных/graphs/DFS/DFS4.py
@@ -1,71 +1,49 @@
-from __future__ import annotations
 from collections import defaultdict
 
-
 class Graph:
+    def __init__(self, V: int) -> None:     # V - количество вершин
+        self.V = V
+        self.adj = defaultdict(list)
 
-    def __init__(self, vertex: int) -> None:
-        self.V = vertex  # вершина
-        self.graph = defaultdict(list)
+    def DFSUtil(self, temp: list[int], v: int, visited: list[int]) -> list[int]:
+        visited[v] = True
+        temp.append(v)
+        for i in self.adj[v]:
+            if not visited[i]:
+                temp = self.DFSUtil(temp, i, visited)
+        return temp
 
-    # Add edge into the graph
-    def add_edge(self, vertex: int, destination: int) -> None:
-        self.graph[vertex].append(destination)
+    def addEdge(self, v:int, w:int) -> None:
+        self.adj[v].append(w)
+        self.adj[w].append(v)
 
-    # dfs
-    def dfs(self, destination: int, visited_vertex: list[bool]) -> None:
-        visited_vertex[destination] = True
-        print(destination, end='')
-        for i in self.graph[destination]:
-            if not visited_vertex[i]:
-                self.dfs(i, visited_vertex)
+    def connectedComponents(self) -> list[list[int]]:
+        visited = [False] * self.V
+        cc = []
+        for v in range(self.V):
+            if not visited[v]:
+                temp = []
+                cc.append(self.DFSUtil(temp, v, visited))
+        return cc
 
-    def fill_order(self, destination, visited_vertex: list[bool], stack: list) -> None:
-        visited_vertex[destination] = True
-        for i in self.graph[destination]:
-            if not visited_vertex[i]:
-                self.fill_order(i, visited_vertex, stack)
-        stack = stack.append(destination)
+# Пример использования
+if __name__ == "__main__":
+    g = Graph(5)
+    g.addEdge(1, 0)
+    g.addEdge(2, 1)
+    g.addEdge(3, 4)
+    cc = g.connectedComponents()
+    print("Компоненты связности графа")
+    print(cc)
 
-    # transpose the matrix
-    def transpose(self) -> Graph:
-        g = Graph(self.V)
+# ввод с клавиатуры
 
-        for i in self.graph:
-            for j in self.graph[i]:
-                g.add_edge(j, i)
-        return g
-
-    # Print stongly connected components
-    def print_scc(self) -> None:
-        stack = []
-        visited_vertex = [False] * (self.V)
-
-        for i in range(self.V):
-            if not visited_vertex[i]:
-                self.fill_order(i, visited_vertex, stack)
-
-        gr = self.transpose()
-
-        visited_vertex = [False] * (self.V)
-        scc_count = 0
-        while stack:
-            i = stack.pop()
-            if not visited_vertex[i]:
-                gr.dfs(i, visited_vertex)
-                print("")
-                scc_count += 1
-        print(f"Количество компонент сильной связности: {scc_count}")
-
-
-# ввод графа с клавиатуры
-num_vertices = int(input("Укажите количество вершин: "))
-g = Graph(num_vertices)
-
-for i in range(num_vertices):
-    vertices = list(map(int, input(f"Укажите вершины, соединенные с вершиной {i}: ").split()))
-    for vertex in vertices:
-        g.add_edge(i, vertex)
-
-print("Компоненты сильной связности:")
-g.print_scc()
+# num_vertices = int(input("Укажите количество вершин: "))
+# g = Graph(num_vertices)
+#
+# for i in range(num_vertices):
+#     vertices = list(map(int, input(f"Укажите вершины, соединенные с вершиной {i}: ").split()))
+#     for vertex in vertices:
+#         g.addEdge(i, vertex)
+#
+# print(g.connectedComponents())
